7d3daee230171c6c9d0dd72bcda3da50a5d86fee
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-25 18:52:51 +0300

    fix error

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe EntitySpec, type: :model do
+RSpec.describe Entity, type: :model do
   describe 'entity model' do
     let(:user) { User.create(name: 'John', email: 'john@example.com', password: 'password123') }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UserSpec, type: :model do
+RSpec.describe User, type: :model do
   describe 'User model ' do
     it ' is valid with valid attributes ' do
       user = User.new(
