b381fc1c204d8321df79e7d15415fa7a2716e94c
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-25 17:37:38 +0300

    Update group_spec_spec.rb

--- spec/models/group_spec_spec.rb
+++ spec/models/group_spec_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe GroupSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  let(:user) { User.create(name: 'John', email: 'john@example.com', password: 'password123') }
+
+  it 'should have an integer user_id' do
+    group = Group.new(user_id: user.id, name: 'food', icon: 'icon')
+    group.user_id = nil
+    expect(group).to_not be_valid
+  end
+
+  it 'name should be presence ' do
+    group = Group.new(user_id: user.id, name: 'food', icon: 'icon')
+    group.name = nil
+    expect(group).to_not be_valid
+  end
+
+  it 'icon should be presence ' do
+    group = Group.new(user_id: user.id, name: 'food', icon: 'icon')
+    group.icon = nil
+    expect(group).to_not be_valid
+  end
