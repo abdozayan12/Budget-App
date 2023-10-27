f194d802acf5a5c3263f1251bb06e57f6ae84121
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-27 11:33:10 +0200

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,4 +10,3 @@ pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/inde
 pin 'stimulus-notification', to: 'https://ga.jspm.io/npm:stimulus-notification@2.2.0/dist/stimulus-notification.mjs'
 pin 'hotkeys-js', to: 'https://ga.jspm.io/npm:hotkeys-js@3.12.0/dist/hotkeys.esm.js'
 pin 'stimulus-use', to: 'https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js'
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
